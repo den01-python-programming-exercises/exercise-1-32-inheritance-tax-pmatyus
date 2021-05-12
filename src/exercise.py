39257e6a170a1210a45c0a280d98b005e2764ac4
pmatyus <>
2021-05-12 21:12:07 +0000

    changed tax to a float

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,25 @@
 def main():
     #write your code below this line
+    inheritance = int(input("Inheritance:"))
+    years = int(input("Years since death:"))
+    taxable = 0
+    tax = 0
+    if (inheritance > 325000):
+      taxable = inheritance - 325000
+    if (years < 3):
+      tax = (taxable *0.4)
+    elif (years < 4):
+      tax = (taxable *0.32)
+    elif (years < 5):
+      tax = (taxable *0.24)
+    elif (years < 6):
+      tax = (taxable *0.16)
+    elif (years < 7):
+      tax = (taxable *0.08)
+    elif (years >= 7):
+      tax = (taxable * 0)
+    print("Tax: " + str(tax))
 
 if __name__ == '__main__':
-    main()
+   main()
+
